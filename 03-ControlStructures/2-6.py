95b220fcd075c19f8023787bdc2983beb079dc0a
Viola Paderina <violapaderina07@gmail.com>
2024-10-18 12:04:25 +0200

    completed section 2

---
+++ 03-ControlStructures/2-6.py
@@ -0,0 +1,7 @@
+number = int(input("Enter the number: "))
+if number > 0:
+    print(f"Number {number} is positive")
+elif number < 0:
+    print(f"Number {number} is negative")
+else:
+    print(f"Number is 0")
