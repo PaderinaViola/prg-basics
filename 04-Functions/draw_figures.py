6730f10a1c299ee3c8c132e90da459a5af18a035
Viola Paderina <violapaderina07@gmail.com>
2024-11-02 12:09:27 +0100

    done 5th section

--- 04-Functions/draw_figures.py
+++ 04-Functions/draw_figures.py
@@ -5,6 +5,11 @@ window = turtle.Screen()
 window.bgcolor("lightgreen")
 
 side_length = 100
+length_a = 200
+length_b = 250
 figures.draw_square(side_length)
+figures.draw_triangle(side_length)
+figures.draw_rectangle(length_a, length_b)
+
 
 window.mainloop()
--- 04-Functions/figures.py
+++ 04-Functions/figures.py
@@ -7,11 +7,41 @@ def draw_square(length):
         pen.forward(length)
         pen.right(90)
     pen.hideturtle()
+    pen.penup()
+    pen.goto(-250, 200)
+    pen.pendown()
+    for _ in range(4):
+        pen.forward(length)
+        pen.right(90)
 
 def draw_triangle(length):
     pen = turtle.Turtle()
     pen.speed(5)
-    for _ in range(4):
+    for _ in range(3):
         pen.forward(length)
+        pen.right(120)
+    pen.hideturtle()
+    pen.penup()
+    pen.goto(-100, 200)
+    pen.pendown()
+    for _ in range(3):
+        pen.forward(length)
+        pen.right(120)
+
+def draw_rectangle(length_a, lenght_b):
+    pen = turtle.Turtle()
+    pen.speed(5)
+    for _ in range(2):
+        pen.forward(length_a)
+        pen.right(90)
+        pen.forward(lenght_b)
         pen.right(90)
     pen.hideturtle()
+    pen.penup()
+    pen.goto(50, 300)
+    pen.pendown()
+    for _ in range(2):
+        pen.forward(length_a)
+        pen.right(90)
+        pen.forward(lenght_b)
+        pen.right(90)
