8be9bf5e16d6e24c527ca95406e384b89df2551b
Viola Paderina <violapaderina07@gmail.com>
2024-12-03 23:46:57 +0100

    done section 3

--- 08-FileHandling/3-3.py
+++ 08-FileHandling/3-3.py
@@ -1,5 +1,10 @@
 import re
-#\w+\s\d{1,2},\s\d{4}
-#\d{3}-\d{3}-\d{4}
-#/\d,\d+/gm
-#
+text = "Alice was born on March 12, 1992. Her brother, John, was born on June 5, 1988. They have a mutual friend named Mike, whose phone number is 555-123-4567. In their hometown, which has a population of 1,234 or 1,235 people, a holiday festival is held every year on December 25. Alice works in an office with 30 employees. Her phone number is 555-765-4321."
+dates = re.findall("\w+\s\d{1,2},\s\d{4}", text)
+print(dates)
+phone_numbers = re.findall("\d{3}-\d{3}-\d{4}", text)
+print(phone_numbers)
+numbers = re.findall("\d,\d+", text)
+print(numbers)
+names = re.findall("\b[A-Z][a-z]+\b", text)
+print(names)
