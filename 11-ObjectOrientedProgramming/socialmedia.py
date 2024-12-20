6ff7ce3273b2edc549822516526b822cdd0a0f16
PaderinaViola <violapaderina07@gmail.com>
2024-12-20 12:25:52 +0100

    done up to 2-4

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,22 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def display_timeline(self):
+        counter = 1
+        print(f"Timeline of {self.username}:")
+        for b in self.posts:
+            print(f"{counter}. {b}")
+            counter += 1
+
+johndoe = SocialMediaProfile("johndoe")
+
+def main():
+    johndoe.add_post("Hello, world!")
+    johndoe.add_post("Had a great day at the park!")
+    johndoe.add_post("What's up, Natalie? How are you?")
+    johndoe.display_timeline()
+
+
+if __name__ == "__main__":
+    main()
+
--- 11-ObjectOrientedProgramming/taxi.py
+++ 11-ObjectOrientedProgramming/taxi.py
@@ -8,12 +8,20 @@ class TaxiRide:
         self.distance = distance
         self.fare = self.distance * self.rate_per_km
 
+    def print_receipt(self):
+        return self.fare
+
+
+
 
 def main():
     # your program
-    ...
-    ...
-    ...
+    ride1 = TaxiRide(13)
+    ride2 = TaxiRide(7)
+    ride1.calculate_fare(9)
+    ride2.calculate_fare(20)
+    print(ride1.print_receipt())
+    print(ride2.print_receipt())
 
 if __name__ == "__main__":
     main()
