0cb57ae203642f2c01d3b4b781a930695c6aedf1
Viola Paderina <violapaderina07@gmail.com>
2025-01-04 21:56:24 +0100

    вone 5th section

--- 11-ObjectOrientedProgramming/tv.py
+++ 11-ObjectOrientedProgramming/tv.py
@@ -3,20 +3,30 @@ class TV:
         self.is_on = False
         self.channel_no = 1
         self.channel_names = []
+        self.volume = 0
 
     def turn_on(self):
         self.is_on = True
 
     def turn_off(self):
         self.is_on = False
+    
+    def incr_volume(self):
+        if self.volume >= 0 and self.volume < 10:
+            self.volume += 1
+
+    def decr_volume(self):
+        if self.volume <= 10 and self.volume > 0:
+            self.volume -= 1
 
     def show_status(self):
-        if 1 <= self.channel_no <= len(self.channel_names):
-            if self.is_on:
+        channel_name = ""
+        if self.is_on:
+            if 1 <= self.channel_no <= len(self.channel_names):
                 channel_name = self.channel_names[self.channel_no - 1]  # дивиться в ареї на якому місці елемент
-                print(f"The TV is on, channel {self.channel_no} ({channel_name})")
-            else:
-                print(f"The TV is off")
+            print(f"The TV is on, channel {self.channel_no} ({channel_name}), volume: {self.volume}")
+        else:
+            print(f"The TV is off")
 
     def set_channel(self, new_channel_no):
         if self.is_on:
